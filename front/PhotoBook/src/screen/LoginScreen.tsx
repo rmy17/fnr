37223d02499ffb664d3de441bb8b588de5a44751
remy VIANNAIS <r.viannais@groupeonepoint.com>
2022-06-08 17:29:15 +0200

    dev: login screen input

--- front/PhotoBook/src/screen/LoginScreen.tsx
+++ front/PhotoBook/src/screen/LoginScreen.tsx
@@ -1,6 +1,6 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {RootStackParamList} from '../navigation';
 
@@ -10,6 +10,18 @@ const LoginScreen = ({navigation}: LoginProps) => {
   return (
     <View style={styles.mainContainer}>
       <Text style={styles.textContainer}>Login screen</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Login"
+        onChangeText={newText => {}}
+        defaultValue={''}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={newText => {}}
+        defaultValue={''}
+        secureTextEntry={true}
+      />
       <Button
         title="CONNECT"
         onPress={() => {
@@ -24,12 +36,20 @@ export default LoginScreen;
 const styles = StyleSheet.create({
   mainContainer: {
     backgroundColor: Colors.dark,
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
     height: '100%',
+    flexDirection: 'column',
+    padding: 10,
   },
   textContainer: {
     fontSize: 50,
     fontWeight: 'bold',
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 5,
+    marginHorizontal: 20,
+    marginVertical: 20,
   },
 });
