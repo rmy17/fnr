83d5eedbee8cf93675a7d47fabcb9c46ee123942
remy VIANNAIS <r.viannais@groupeonepoint.com>
2022-06-10 10:02:30 +0200

    dev:added static image

--- front/PhotoBook/src/screen/LegalScreen.tsx
+++ front/PhotoBook/src/screen/LegalScreen.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const LegalScreen = () => {
   return (
     <View style={styles.mainContainer}>
       <Text style={styles.textContainer}>LegalScreen</Text>
+      <Image style={styles.image} source={require('../../assets/legal.png')} />
     </View>
   );
 };
@@ -22,5 +23,8 @@ const styles = StyleSheet.create({
   textContainer: {
     fontSize: 50,
     fontWeight: 'bold',
+  },
+  image: {
+    width: '100%',
   },
 });
